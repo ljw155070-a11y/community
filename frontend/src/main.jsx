2fd84cf5fdaa0314be09fd2ff432f3ed47781c72
chans0928 <chans0928@gmail.com>
2025-12-17 18:26:02 +0900

    fix : main.jsx에 recoilRoot추가

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
   </StrictMode>
 );
