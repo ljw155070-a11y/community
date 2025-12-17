126c1f0888faa4d33c84fa5412687b25145551a0
yeah~ <skjb6996@naver.com>
2025-12-17 15:45:19 +0900

    feat : 헤더 푸터 csr, ssr 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,22 @@ import { Route, Routes } from "react-router-dom";
 import Signup from "./components/common/Signup";
 import Login from "./components/common/Login";
 import BoardWrite from "./components/board/BoardWrite";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/boardwrite" element={<BoardWrite />} />
-    </Routes>
+    <div className="wrap">
+      <Header></Header>
+      <main className="section">
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/boardwrite" element={<BoardWrite />} />
+        </Routes>
+      </main>
+      <Footer></Footer>
+    </div>
   );
 }
 
