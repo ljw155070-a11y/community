108fa2a044941117045ec6b73ad80bdb5f43e713
JeonHywn <wjsgusdn1tp@naver.com>
2025-12-18 17:13:42 +0900

    feat : 헤더 로그인 수정

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -81,27 +81,6 @@ const Header = () => {
         </nav>
 
         <div className="header-right">
-          <Link className="btn-login" to="/login">
-            로그인
-          </Link>
-          <Link className="btn-signup" to="/signup">
-            회원가입
-          </Link>
-          <a href="/notifications" className="notification-link">
-            <svg
-              className="notification-icon"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-              <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-            </svg>
-            <span className="notification-badge"></span>
-          </a>
           {!loginUser ? (
             <>
               <Link className="btn-login" to="/login">
